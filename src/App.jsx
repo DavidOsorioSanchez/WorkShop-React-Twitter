1a0a81ddaca54c7f82982e3b6be19564c0d98255
David Osorio <daalosx@gmail.com>
2024-09-11 02:27:33 -0500

    terminando minimo viable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "@components/sidebar/Sidebar.jsx"
 import Main from "@components/main/main"
 import Actionbar from "@components/actionbar/Actionbar.jsx"
+import Bloqueador from "@components/other/Bloqueador.jsx"
 export default function App() {
   return (
     <div className="app">
       <Sidebar />
       <Main/>
       <Actionbar/>
+      <Bloqueador />
     </div>
   )
 }
---
+++ src/components/other/Bloqueador.jsx
@@ -0,0 +1,8 @@
+import "./bloqueador.css"
+export default function Blocker() {
+    return (
+        <div className="blocker">
+            <p>ni movil ni tablet no disponible</p>
+        </div>
+    )
+}
